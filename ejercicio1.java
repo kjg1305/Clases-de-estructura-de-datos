9e7a6d21c849400805ff7d0b5d779817dbe8df0f
kj <karen@gmail.com>
2025-08-17 11:25:50 -0500

    Relase: first commit

---
+++ ejercicio1.java
@@ -0,0 +1,26 @@
+public class ejercicio1 {
+    public static void main (String[]args){
+        int[] numeros = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 20;
+        numeros[2] = 30;
+        numeros[3] = 40;
+        numeros[4] = 50;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Elemento en la posición " + i + ": " + numeros[i]);
+        }
+        float[] decimales = new float[3];
+        decimales[0] = 1.1f;
+        decimales[1] = 2.2f;
+        decimales[2] = 3.3f;
+         
+
+        System.err.println(" hice algo ");
+        System.err.println("cambie de rama ");
+        System.err.println("cambie a otra rama");
+        System.err.println("cambie a otra rama por ultima vez");
+       
+
+    }
+}
